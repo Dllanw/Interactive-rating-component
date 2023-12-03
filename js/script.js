1a50ff43c7aa32f7aa5c9b5537a6baae07ae81ff
Dylan 7n7 <dcreyes0920@gmail.com>
2023-12-02 19:43:12 -0600

    js2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const stars = document.querySelectorAll(".star");
 stars.forEach(function(star) { //Iteramos sobre cada elemento
     star.addEventListener("click", function() { 
-        console.log("diste click");
+        //Elimina la clase "star-selected" de todas las estrellas
+        stars.forEach(function(s) {
+            s.classList.remove("star-selected");
+        });
+        //Agrega la clase "star-selected" a la estrella clicada
+        this.classList.add("star-selected");
     });
 })
