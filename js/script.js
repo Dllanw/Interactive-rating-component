fceadde4e2820d463023cbfa6455e6971fd3d1ae
Dylan 7n7 <dcreyes0920@gmail.com>
2023-12-10 21:20:52 -0600

    v3

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-const stars = document.querySelectorAll(".star");
-const rating = document.querySelector("#rating");
+document.addEventListener('DOMContentLoaded', function() {
 
-stars.forEach(function(star) { //Iteramos sobre cada elemento
-    star.addEventListener("click", function() { 
-        //Elimina la clase "star-selected" de todas las estrellas
-        stars.forEach(function(s) {
-            s.classList.remove("star-selected");
+    const stars = document.querySelectorAll(".star");
+    const rating = document.querySelector("#rating");
+    
+    stars.forEach(function(star) { //Iteramos sobre cada elemento
+        star.addEventListener("click", function() { 
+            //Elimina la clase "star-selected" de todas las estrellas
+            stars.forEach(function(s) {
+                s.classList.remove("star-selected");
+            });
+            //Agrega la clase "star-selected" a la estrella clicada
+            this.classList.add("star-selected");
+            const selectStar = star
+            updateRating(selectStar)
+            
+            
         });
-        //Agrega la clase "star-selected" a la estrella clicada
-        this.classList.add("star-selected");
-    });
+    })
+
+    function updateRating(selectRating) {
+        rating.textContent = selectRating;
+    }
+
 })
