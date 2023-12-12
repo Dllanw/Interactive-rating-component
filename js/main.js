1800caae0508657b1074d43649853d772aea736c
Dylan 7n7 <dcreyes0920@gmail.com>
2023-12-11 18:37:25 -0600

    v5

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
     
     const score = document.querySelector("#rating");
-    const rate = document.contentDocument('.card__info');
-    console.log(rate)
+    const rate = document.querySelector('#rate');
+    
     
 
     score.addEventListener('click', function(e) {
-        const selectedRating = (e.target.innerText);
-        localStorage.setItem('selectedRating', selectedRating);
-        window,location.href = 'thank-you.html';
+       rate.innerText = e.target.innerText;
     })
 
 
