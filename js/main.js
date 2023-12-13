406edb81cc6f2d47c46e4853df652a9db8109948
Dylan 7n7 <dcreyes0920@gmail.com>
2023-12-12 20:38:09 -0600

    Version-Final

--- js/main.js
+++ js/main.js
@@ -3,20 +3,26 @@ document.addEventListener("DOMContentLoaded", function(){
     const score = document.querySelector("#rating");
     const rate = document.querySelector('#rate');
     const submit = document.querySelector('#btn'); 
-    const card = document.querySelector('.card__thanyou')
+    const cardTy = document.querySelector('.card__thanyou')
+    const cardRating = document.querySelector('.card')
     
 
     score.addEventListener('click', function(e) {
        rate.innerText = e.target.innerText;
-       
+       if (e.target.classList.contains('click')) {
+           submit.classList.remove('opacity');
+           
+       } else if(e.target) {
+           submit.classList.add('opacity')
+
+       };
     });
     submit.addEventListener('click', function(e){
-        card.classList
-        console.log(e);
-        
+        if (!submit.classList.contains('opacity')) {
+            cardRating.style.display = 'none';
+            cardTy.style.display = 'grid';
+            return;
+        };
     });
 
-
-
-
-})
+});
