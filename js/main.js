8480d5bbfd1cc46ed40b2372dbd6293ca3166359
Dylan 7n7 <dcreyes0920@gmail.com>
2023-12-12 19:12:47 -0600

    V6

--- js/main.js
+++ js/main.js
@@ -2,12 +2,20 @@ document.addEventListener("DOMContentLoaded", function(){
     
     const score = document.querySelector("#rating");
     const rate = document.querySelector('#rate');
-    
+    const submit = document.querySelector('#btn'); 
+    const card = document.querySelector('.card__thanyou')
     
 
     score.addEventListener('click', function(e) {
        rate.innerText = e.target.innerText;
-    })
+       
+    });
+    submit.addEventListener('click', function(e){
+        card.classList
+        console.log(e);
+        
+    });
+
 
 
 
